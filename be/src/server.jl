92d56b9500269eb3f7156dae7bb02a5df4424ebf
Mark Halonen <halonen.mark@gmail.com>
2019-04-13 14:34:45 -0400

    this is so fricken cool

--- be/src/server.jl
+++ be/src/server.jl
@@ -1,42 +1,46 @@
-println("server starting!")
 using Joseki
-println("Joseki imported!")
 using JSON
-println("JSON imported!")
 using HTTP
-println("HTTP imported")
 import HTTP.IOExtras.bytes
-using SaunaModel:json_api
+println("Starting sauna import")
+using SaunaModel:dictionary_api
+using SaunaModel:get_default_scenario_json
 println("SaunaModel imported!")
 ### Create some endpoints
 
-# This function takes two numbers x and y from the query string and returns x^y
-# In this case they need to be identified by name and it should be called with
-# something like 'http://localhost:8000/pow/?x=2&y=3'
-function pow(req::HTTP.Request)
-    j = HTTP.queryparams(HTTP.URI(req.target))
-    has_all_required_keys(["x", "y"], j) || return error_responder(req, "You need to specify values for x and y!")
-    # Try to parse the values as numbers.  If there's an error here the generic
-    # error handler will deal with it.
-    x = parse(Float32, j["x"])
-    y = parse(Float32, j["y"])
-    json_responder(req, x^y)
-end
-
-# This function takes two numbers n and k from a JSON-encoded request
-# body and returns binomial(n, k)
-function bin(req::HTTP.Request)
-    j = try
-        body_as_dict(req)
-    catch err
-        return error_responder(req, "I was expecting a json request body!")
+function dotNotationToDict(params::Dict)
+    result = Dict()
+    for (key, value) in params
+        # key: sauna.stove.mass
+        split_keys = split(key, ".")
+        current_dict = result
+        for key_idx in 1:length(split_keys)
+            sub_key = split_keys[key_idx]
+            if key_idx == length(split_keys)
+                current_dict[sub_key] = parse(Float64, value)
+            elseif !haskey(current_dict, sub_key) 
+                current_dict[sub_key] = Dict()
+                current_dict = current_dict[sub_key]
+            else 
+                current_dict = current_dict[sub_key]
+            end
+            
+        end
     end
-    has_all_required_keys(["n", "k"], j) || return error_responder(req, "You need to specify values for n and k!")
-    json_responder(req, binomial(j["n"],j["k"]))
+    return result
 end
 
 function simulate(req::HTTP.Request)
-    b = json_api("{}")
+    params = HTTP.queryparams(HTTP.URI(req.target))
+    # params = Dict(String -> String)
+    dict_format = dotNotationToDict(params)
+    b = JSON.json(dictionary_api(dict_format))
+    req.response.body = bytes(b)
+    return req.response
+end
+
+function params(req::HTTP.Request)
+    b = get_default_scenario_json()
     req.response.body = bytes(b)
     return req.response
 end
@@ -45,9 +49,8 @@ end
 
 # Make a router and add routes for our endpoints.
 endpoints = [
-    (pow, "GET", "/pow"),
-    (bin, "POST", "/bin"),
-    (simulate, "GET", "/simulate")
+    (simulate, "GET", "/simulate"),
+    (params, "GET", "/params")
 ]
 r = Joseki.router(endpoints)
 
