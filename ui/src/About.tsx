1d55ce809057e9fbd79b6840b1a025497fb80558
Mark Halonen <halonen.mark@gmail.com>
2019-04-17 19:03:13 -0400

    about page updates

--- ui/src/About.tsx
+++ ui/src/About.tsx
@@ -8,23 +8,26 @@ class About extends React.Component {
     public render() {
         return (
             <div style={{ padding: 25 }}>
-                <Typography>SaunaSim originated when Mark commissioned the construction of a trailer sauna. While installing a water tank, Mark asked a group chat about the thermodynamics of adding a water tank. The resulting discussion evolved into SaunaSim.</Typography>
+                <Typography>SaunaSim originated when Mark commissioned the construction of a trailer sauna. Mark asked a group chat about the thermodynamics of a water tank, and the resulting discussion evolved into SaunaSim.</Typography>
                 <Typography variant="title" style={{ paddingTop: 10, paddingBottom: 5 }}>Creators</Typography>
                 <div style={{ paddingLeft: 10 }}>
-                    <Typography>Brent Halonen created the model in Julia. <Link
+                    <Typography><strong>Brent Halonen</strong> created the simulator (<Link
                         href={"https://github.com/bhalonen/SaunaModel.jl"}
                         target="_blank"
                     >
                         SaunaModel.jl
-                </Link>
+                </Link>) in Julia, a programming language that aims to be "A fresh approach to technical computing".
                     </Typography>
-                    <Typography>Mark Halonen created the web server in Julia and this website in React. <Link
+                    <Typography><strong>Mark Halonen</strong> created this website (<Link
                         href={"https://github.com/markhalonen/sauna-sim"}
                         target="_blank"
                     >
                         sauna-sim
-                </Link></Typography>
+                </Link>) using Julia for the server and React for the UI. </Typography>
+
                 </div>
+                <Typography variant="title" style={{ paddingTop: 30, paddingBottom: 5 }}>How a wood sauna works</Typography>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/fzaNYXj-7uQ" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true} />
             </div>
         )
     }
